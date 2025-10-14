dffc6e36bd14829dfd659962971e593bf275bcd5
jamal <znac510>
2025-10-14 23:21:59 +0100

    adding class relates to #1

---
+++ classes.py
@@ -0,0 +1,12 @@
+## create database saver for students
+
+class student:
+    def __init__(self,name,ID,year,classes):
+        self.name = name
+        self.ID = ID
+        self.year = year
+        self.classes = classes
+    
+
+
+
