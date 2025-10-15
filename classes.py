5dc57a919ebf504702a825343a8f9a44af32955f
jamal <znac510>
2025-10-15 18:47:58 +0100

    added to classes.py

--- classes.py
+++ classes.py
@@ -1,12 +1,27 @@
 ## create database saver for students
-
+import sqlite3
 class student:
     def __init__(self,name,ID,year,classes):
         self.name = name
         self.ID = ID
         self.year = year
         self.classes = classes
-    
+        self.adding()
+    def adding(self):
+        try:
+            conn = sqlite3.connect('mydatabase.db')
+        except:
+            print("error connecting")
+        try:
+            conn.execute("INSERT INTO students(name,id,year) VALUES (?,?,?)",(self.name,self.ID,self.year))
+        except:
+            print("error adding to database")
+        
+        conn.commit()
+        conn.close()
+        print("hello")
 
 
+
+jeff = student("jeff",777,2025,any)
 
