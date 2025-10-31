19c6be48b48fd9b423348a8c1c93c66a9bd2fd25
jamal <znac510>
2025-10-31 15:46:04 +0000

    #5 adding to tests

--- Tests.py
+++ Tests.py
@@ -3,10 +3,11 @@
 
 ##test user
 from Student import student
-from searcher import getting_specific
-
+import searcher
+##for tests
 jeff = student("jeff",777,2025)
-
+claire = student("claire",777,2025)
+brianna = student("brianna",778,2026)
 
 def add():
     jeff.adding()
@@ -24,4 +25,32 @@ def add_duplicate():
 
 add()
 
-getting_specific()
+searcher.getting_specific()
+
+def Test1():
+    #testing adding to database
+    jeff.adding()
+    brianna.adding()
+
+def Test2():
+    #test adding same Id to database
+    claire.adding()
+    ##should return an error
+def Test3():
+    #test adding a module
+    module = "science"
+    grade = 67
+    jeff.addmodule(module,grade)
+
+def Test4():
+    #testing searcher/py
+    searcher.getting_all()
+    ID = input("enter a ID: ")
+    searcher.getting_specific()
+    searcher.getting_modules_grades(ID)
+
+if __name__ == "__main__":
+    Test1()
+    Test2()
+    Test3()
+    Test4()
