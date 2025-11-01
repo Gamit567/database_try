37978117781aecf25d5b8a56552e53c084ee3e72
jamal <znac510>
2025-11-01 18:32:03 +0000

    #9 making a user interface

--- userinterface.py
+++ userinterface.py
@@ -1,14 +1,42 @@
 
 import tkinter as tk
-screen = tk.Tk()
-
-
-screen.title("database system")
-enterstudent = tk.Label(screen,text='WELCOME').pack()
-enterstudent = tk.Button(screen,text='enter student').pack()
-findstudent = tk.Button(screen,text='find student').pack()
 
 
 
+class interface:
+    def __init__(self):
+        #making the main screen
+        self.screen = tk.Tk()
+        self.screen.geometry("500x250")
+        #making the frames
+        self.main = tk.Frame(self.screen)
+        self.enter = tk.Frame(self.screen)
+        self.find = tk.Frame(self.screen)
+        self.all_buttons()
+        self.inital_buttons()
+    def all_buttons(self):
+        #making the buttons
+        self.welcomescreen = tk.Label(self.main,text='WELCOME')
+        self.enterstudent = tk.Button(self.main,text='enter student',width=25,height=2,command=self.enterscreen)
+        self.findstudent = tk.Button(self.main,text='find student',width=25,height=2,command=self.findscreen)
+    def inital_buttons(self):
+        # makes other frames empty
+        self.enter.forget()
+        self.find.forget()
+        #getting main frame
+        self.main.pack()
+        self.screen.title("database system")
+        #intializing initial buttons
+        self.welcomescreen.pack()
+        self.enterstudent.pack()
+        self.findstudent.pack()
 
-screen.mainloop()
+    def enterscreen(self):
+        self.main.forget()
+        self.enter.pack()
+    def findscreen(self):
+        self.main.forget()
+        self.find.pack()
+
+s = interface()
+s.screen.mainloop()
