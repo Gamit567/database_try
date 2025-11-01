d36f42016f445c15a0053ad62d8c742cc756ead2
jamal <znac510>
2025-11-01 17:42:34 +0000

    #9 initial interface

---
+++ userinterface.py
@@ -0,0 +1,14 @@
+
+import tkinter as tk
+screen = tk.Tk()
+
+
+screen.title("database system")
+enterstudent = tk.Label(screen,text='WELCOME').pack()
+enterstudent = tk.Button(screen,text='enter student').pack()
+findstudent = tk.Button(screen,text='find student').pack()
+
+
+
+
+screen.mainloop()
